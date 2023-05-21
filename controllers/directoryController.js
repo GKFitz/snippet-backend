1070406c3738803c53419234bf41a4f5256c25df
GKFitz123 <gkf2103@gmail.com>
2023-05-20 23:06:25 -0400

    working on the directories to frontend

--- controllers/directoryController.js
+++ controllers/directoryController.js
@@ -3,7 +3,7 @@ const Directory = require('../models/directory.js')
 const mongoose = require('mongoose')
 
 
-//GET all directories
+//GET ALL directories
 
 const getDirectories = async (req, res) => {
     const directories = await Directory.find({}).sort({createdAt: -1})
@@ -12,7 +12,15 @@ const getDirectories = async (req, res) => {
 }
 
 
-//GET a single Directory
+//GET ALL Snippets on a Directory
+const getSnippets = async (req, res) => {
+    // const directories = await Directory.find({}).sort({createdAt: -1})
+    const id = req.params.id
+    const {snippets} = await Directory.findById(id)
+    res.status(200).json(snippets)
+}
+
+//GET ONE Directory
 const getDirectory = async (req, res) => {
     const { id } = req.params
 
@@ -28,6 +36,132 @@ const getDirectory = async (req, res) => {
     res.status(200).json(directory)
 }
 
+// GET ONE Snippet from a Directory
+const getOneSnippet = async (req, res) => {
+    const { id } = req.params;
+    const { snippets } = req.params.snippets;
+    await Directory
+        .findById(id)
+        .select('snippets')
+        .exec(function (err, directory) {
+            let response = { };
+      
+            if (err) {
+              response = responseDueToError(err);
+            } else if (!directory) {
+              response = responseDueToNotFound();
+            } else {
+              let snippets = directory.snippets.id(id);
+      
+              if (snippets === null) {
+                response = responseDueToNotFound();
+              } else {
+                response.status = HttpStatus.OK;
+                response.message = snippets;
+              }
+            }
+      
+            res.status(response.status).json(response.message);
+          });
+}
+
+
+
+// //DELETE a Directory
+// const deleteDirectory = async (req, res) => {
+//     const { id } = req.params
+
+//     if (!mongoose.Types. ObjectId.isValid(id)) {
+//         return res.status(404).json({error: 'No such directory'})
+//     }
+
+//     const directory = await Directory.findOneAndDelete({_id: id})
+
+//     if (!directory) {
+//         return res.status(404).json({error: 'No such directory'})
+//     }
+//     res.status(200).json(directory)
+
+// }
+
+// DELETE a Directory
+const deleteDirectory = async (req, res) => {
+    try{
+    const { id }= req.params
+    const directory = await Directory.findOneAndUpdate(
+    )
+    res.json(directory)
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+// DELETE a Snippet from Directory
+// const deleteSnippet = async (req, res) => {
+//     const {id, snippets_index} = req.params;
+//     const directory = await Directory.findOne({_id: (id)}
+//     if (err) {
+//         console.error("cannot get user", err)
+//         res.status(404)
+//     } else {
+//         const snippet = directory.snippets[snippets_index];
+
+//         const directory = Directory.findOneAndUpdate(
+
+//         )
+
+//     }
+    
+
+//     )
+
+
+// }
+
+
+
+  
+//UPDATE a whole Directory
+const updateDirectory = async (req, res) => {
+    try{
+    const { id }= req.params
+    const { snippets } = req.body
+    console.log(snippets)
+    console.log(Directory)
+    const directory = await Directory.findOneAndUpdate(
+        { _id: (id) }, 
+        { $push: { "snippets": {$each:snippets }}}, 
+        { new: true },
+    )
+    res.json(directory)
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+
+//UPDATE/ADD a Snippet to a Directory
+// const addSnipToDirectory = async (req, res) => {
+//     try{
+//       const snippets  = req.body
+//       const { id, snipId }= req.params
+//       console.count()
+//     //   console.log(snippets)
+//       console.log(Directory)
+//       const directory= await Directory.findOneAndUpdate(
+//         { _id: (id) }, 
+//         { $set: {[`snippets.${snipId}`]: snippets,},},
+//         // { $set: {'snippets.0.description': 'something', 'snippets.0.codeSnip': 'console.log(1)'} 
+//         { new: true }
+//     )
+//     res.status(200).json(directory)
+//     }catch (error){
+//         res.status(400).json({error: error.message})
+//     }
+
+// }
+
 
 //CREATE a Directory
 const createDirectory = async (req, res) => {
@@ -42,102 +176,6 @@ const createDirectory = async (req, res) => {
     }
 
 }
-
-//DELETE a Directory
-const deleteDirectory = async (req, res) => {
-    const { id } = req.params
-
-    if (!mongoose.Types. ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such directory'})
-    }
-
-    const directory = await Directory.findOneAndDelete({_id: id})
-
-    if (!directory) {
-        return res.status(404).json({error: 'No such directory'})
-    }
-    res.status(200).json(directory)
-
-}
-
-//UPDATE a Directory
-
-// const updateDirectory = async (req, res) => {
-//     try{
-//     const {id} = req.params;
-//     const { snippets } = req.body
-//     console.log(snippets)
-//     console.log(Directory)
-//     await Directory.findOneAndUpdate(
-//         { _id: ObjectId(id) },
-//         { $push: { "snippets": {$each:snippets}}},
-//         { new: true },
-
-        
-       
-//     )}catch (error){
-//         res.status(400).json({error: error.message})
-//     }
-    
-// }     
-  
-//UPDATE a Directory WITH a snippet
-const updateDirectory = async (req, res) => {
-    try{
-         
-      const { id }= req.params
-      const { snippets } = req.body
-    
-      console.count()
-      console.log(snippets)
-      console.log(Directory)
-      const directory = await Directory.findOneAndUpdate(
-        { _id: (id) }, 
-        { $push: { "snippets": {$each:snippets }}}, 
-        { new: true },
-    )
-     res.json(directory)
-    }catch (error){
-        res.status(400).json({error: error.message})
-    }
-
-}
-
-// const deleteDirectory = async (req, res) => {
-//     try{
-//         const { id }= req.params
-//         const directory = await Directory.findOneAndUpdate(
-          
-//       )
-//        res.json(directory)
-//       }catch (error){
-//           res.status(400).json({error: error.message})
-//       }
-// }
-
-//UPDATE a Directory WITH a snippet
-const addSnipToDirectory = async (req, res) => {
-    try{
-      const { title } = req.body
-      const { id, snippets_index }= req.params
-      console.count()
-      console.log(snippets)
-      console.log(Directory)
-      const directory= await Directory.findOneAndUpdate(
-        { _id: (id) }, 
-        { $set: {[`snippets.${snippets_index}.title`]: title,},}, 
-        { new: true }
-    )
-    res.status(200).json(directory)
-    }catch (error){
-        res.status(400).json({error: error.message})
-    }
-
-}
-
-const de
-
-
 
 
 
@@ -196,7 +234,10 @@ module.exports = {
     createDirectory,
     deleteDirectory,
     updateDirectory,
-    addSnipToDirectory
+    addSnipToDirectory,
+    getSnippets,
+    getOneSnippet
+
     // updateSnippetDir
 
 }
--- routes/directory.js
+++ routes/directory.js
@@ -8,9 +8,11 @@ const {
     getDirectory,
     createDirectory,
     deleteDirectory,
+    deleteSnippet,
     updateDirectory,
-    addSnipToDirectory
-    // updateSnippetDir
+    addSnipToDirectory,
+
+    
 
 
 } = require('../controllers/directoryController.js')
@@ -21,14 +23,23 @@ const router = express.Router()
 
 
 
-//GET all Directories
+//GET ALL Directories
 router.get('/', getDirectories)
 //     // res.json({message: 'GET ALL DIRECTORIES'})
 
+//GET ALL Snippets on a particular directory
+router.get('/:id/snippets', getSnippets)
+    // res.json({message: 'GET ALL DIRECTORIES'})
 
-// //GET one Directory
+
+// //GET ONE Directory
 router.get('/:id', getDirectory)
 //     // res.json({message: 'GET one DIRECTORY'})
+
+//GET ONE Snippet on a Directory
+router.get('/:id/snippet/:snippets_index', getOneSnippet)
+//     // res.json({message: 'GET one DIRECTORY'})
+
 
 
 //POST a new Directory
@@ -36,24 +47,32 @@ router.post('/', createDirectory)
     // res.json({message: 'POST A New DIRECTORY'})
 
 
-//Delete a Directory
+//DDELETE a Directory
 router.delete('/:id', deleteDirectory)
     // res.json({message: 'GET ALL DIRECTORIES'})
 
+
+//DELETE a Snippet from Snippet Array on a Directory
+// router.delete("/id/snippets/:snippets_index", deleteSnippet)
 
 //UPDATE a Directory
 router.put('/:id', updateDirectory)
     // res.json({ message: 'UPDATE a DIRECTORY'})
 
 
+//UPDATE/ADD a Snippet to Directory
+// router.put('/:id/snippets/:snipId', addSnipToDirectory)
+
+
+
+
 // router.route('/:id/snippet')
 //     // .post(createSnippet)
 //     .put(updateSnippetDir)
 
-router.put('/:id/snippet/:snippets_index/title', addSnipToDirectory)
-    // res.json({ message: 'UPDATE a DIRECTORY'})
 
 
+// router.put('/:id/snippet/:snippets_index')
 
 
 module.exports = router;
