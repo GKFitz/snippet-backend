3dc92ec4feb0deb85c673b21cb4387507573f968
GKFitz123 <gkf2103@gmail.com>
2023-05-21 23:04:17 -0400

    clean up snippet routes, and readded the router get for getASnippet

--- routes/snippet.js
+++ routes/snippet.js
@@ -2,58 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 
-// const {
-//     // getASnippet,
-//     // updateSnippetDir,
-//     // updateSnippet,
-//     // getOneSnippet,
-//     // createSnippet,
-//     // deleteSnippet
-//     // updateDirectory
+const {
+    getASnippet,
+    // updateSnippet,
+    // deleteSnippet
+    // updateDirectory
 
-// } = require('../controllers/snippetControllers.js')
+} = require('../controllers/snippetControllers.js')
 
+router.get('/:id', getASnippet)
 
 
 
-// router.route('/:dirId/snippet')
-//     // .get(getSnippets)
-//     // // .post(createSnippet)
-//     // .put(updateSnippetDir)
-
-// // update and add a snip to the directory
-// router.route('/:dirId/snippet/:id')
-//     // .get(getOneSnippet)
-//     // .delete(deleteSnippet)
-//     // .put(updateSnippetDir)
-//     // .put(updateSnippetDir)
-
-
-
-//     // router.route('/:dirId/snippet/:id')
-// //     .get(getSnippet)
-// //     .delete(deleteSnippet)
-// //    .put(updateSnippet)
-           
-    
-// router.get('/:id', getASnippet)
-
-// //GET all Directories
-// // router.get('/:dirId/snippet', getSnippets)
-// //     // res.json({message: 'GET ALL DIRECTORIES'})
-
-
-// // //GET one Directory
-// // router.get('/:dirId/snippet/:id', getDirectory)
-// // //     // res.json({message: 'GET one DIRECTORY'})
-
-
-// //POST a new Directory
-// // router.post('/:dirId/snippet', createDirectory)
-//     // res.json({message: 'POST A New DIRECTORY'})
-
-
-// //Delete a Directory
+// //Delete a Snippet
 // router.delete('/delete/:id', deleteSnippet)
     
 
